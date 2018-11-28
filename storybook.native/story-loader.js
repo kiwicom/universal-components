667fb105cadde2a913f92c5e0b258877f267873e
Robin Cussol <me@robincussol.com>
2018-11-28 10:44:12 +0100

    Add: Badge component

--- storybook.native/story-loader.js
+++ storybook.native/story-loader.js
@@ -1,25 +1,29 @@
-// @flow
+
 // Auto-generated file created by react-native-storybook-loader
 // Do not edit.
 //
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../src/Badge/Badge.stories');
   require('../src/Button/Button.stories');
-  require('../src/Icon/Icon.stories');
   require('../src/Checkbox/Checkbox.stories');
+  require('../src/Icon/Icon.stories');
   require('../src/ServiceLogo/ServiceLogo.stories');
   require('../src/Text/Text.stories');
   require('../src/TextInput/TextInput.stories');
+  
 }
 
 const stories = [
+  '../src/Badge/Badge.stories',
   '../src/Button/Button.stories',
-  '../src/Icon/Icon.stories',
   '../src/Checkbox/Checkbox.stories',
+  '../src/Icon/Icon.stories',
   '../src/ServiceLogo/ServiceLogo.stories',
   '../src/Text/Text.stories',
   '../src/TextInput/TextInput.stories',
+  
 ];
 
 module.exports = {
